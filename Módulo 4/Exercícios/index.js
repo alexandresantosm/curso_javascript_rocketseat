318b136fdf268ef9f7f9378b1de36d6f0d086ef8
alexandre <amenezes031@gmail.com>
2020-01-09 16:26:33 -0300

    Resolution of exercise 01.

---
+++ Módulo 4/Exercícios/index.js
@@ -0,0 +1,17 @@
+function checaIdade(idade) {
+  return new Promise(function(resolve, reject) {
+    if (idade >= 18) {
+      resolve();
+    } else {
+      reject('Erro na requisição');
+    }
+  });
+}
+
+checaIdade(20)
+  .then(function(response) {
+    console.log('Maior que 18');
+  })
+  .catch(function(error) {
+    console.log('Menor que 18');
+  });
