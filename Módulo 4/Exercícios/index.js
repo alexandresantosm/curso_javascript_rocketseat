3299df8009d7f5de817b0d232639e50df10d40b1
alexandre <amenezes031@gmail.com>
2020-01-09 17:52:42 -0300

    Resolution of exercise 03

--- Módulo 4/Exercícios/index.js
+++ Módulo 4/Exercícios/index.js
@@ -2,9 +2,20 @@ var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 var listElement = document.querySelector('#app ul');
 
+inputElement.onclick = function() {
+  clearInput();
+  var repositoryElement = document.createElement('li');
+  var repositoryText = document.createTextNode('Carregando...');
+
+  repositoryElement.appendChild(repositoryText);
+  listElement.appendChild(repositoryElement);
+}
+
 function findRepository() {
   axios.get(' https://api.github.com/users/'+ inputElement.value +'/repos')
     .then(function(response) {
+      clearInput();
+
       var repositorys = response.data;
 
       for (repository of repositorys) {
@@ -18,7 +29,9 @@ function findRepository() {
       console.log(response.data);
     })
     .catch(function(error) {
-      console.log(error);
+      clearInput();
+
+      alert('Erro 404: '+ error);
     });
 
   clearInput();
@@ -26,4 +39,5 @@ function findRepository() {
 
 function clearInput() {
   inputElement.value = '';
+  listElement.innerHTML = '';
 }
